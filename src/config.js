959009779cdacf7c498c60febb1543e629af4a3f
Jamoliddin <75984860+jamoliddinrikhsiyev@users.noreply.github.com>
2021-07-28 18:37:24 +0500

    updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
-const getIPAddress = require('./lib/getIp.js')
-const IP = getIPAddress(false)
-const PORT = 5500
-const host = "localhost"
+const getIPAddress = require('./lib/getIp.js');
+const IP = getIPAddress(false);
+const PORT = process.env.PORT || 5050;
+const host = "localhost";
 
 module.exports={
 	PORT,
